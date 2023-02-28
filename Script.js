2aa9c99ca5cbd9d8e907d29b6100dd2f79616287
Rupok Adin <t00716058@mytru.ca>
2023-02-28 14:56:23 -0800

    add computerchoice function (not debugged yet)

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
 console.log("Hello World!")
+// variables
+
+let rock;
+let paper;
+let scissors;
+// Randomly return rock, paper or scissors
+
+function getComputerChoice(){
+    let randomNumber = Math.random(0.2)*10;
+    if (randomNumber == 0){
+        return console.log("Rock!");
+    }
+    else if (randomNumber == 1){
+        return console.log("Paper!");
+    }
+    else {
+        return console.log("Scissors!");
+    }
+    
+}
+getComputerChoice()
