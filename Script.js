23ec9ed549ebd6a45161bb95e348529430f1ac7a
Rupok Adin <t00716058@mytru.ca>
2023-03-08 10:33:16 -0800

    add parameters to playRound and mini debugs

--- Script.js
+++ Script.js
@@ -1,4 +1,5 @@
 console.log("Hello World!")
+
 // variables
 
 let rock;
@@ -11,16 +12,19 @@ let computerSelection;
 function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
     if (randomNumber == 0){
-        return computerSelection = "Rock!";
+        return "rock";
     }
     else if (randomNumber == 1){
-         return computerSelection = "Paper!";
+         return "paper";
     }
     else {
-         return  computerSelection = "Scissors!";
+         return "scissors";
     }
     
 }
+
+// Get computer's selection
+computerSelection = getComputerChoice();
 
 // Get player's selection
 
@@ -30,31 +34,40 @@ playerSelection = playerSelection.toLowerCase();
 // find the winner
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
+    if (playerSelection == "rock" && computerSelection == "rock"){
         return alert("That's a draw!");
     }
-    else if (playerSelection == rock && computerSelection == paper){
+    else if (playerSelection == "paper" && computerSelection == "paper"){
+        return alert("That's a draw!");
+    }
+    else if (playerSelection == "scissors" && computerSelection == "scissors"){
+        return alert ("That's a draw!")
+    }
+    else if (playerSelection == "rock" && computerSelection == "paper"){
         return alert("You lose! Paper beats Rock!!");
 
     }
-    else if (playerSelection == rock && computerSelection == scissors){
+    else if (playerSelection == "rock" && computerSelection == "scissors"){
         return alert("You win! Rock beats Scissors!");
 
     }
-    else if (playerSelection == paper && computerSelection == rock){
+    else if (playerSelection == "paper" && computerSelection == "rock"){
         return alert("You win! Paper beats Rock!!");
     }
-    else if (playerSelection == paper && computerSelection == scissors){
+    else if (playerSelection == "paper" && computerSelection == "scissors"){
         return alert("You lose! Scissors beat Paper!");
     }
-    else if (playerSelection == scissors && computerSelection == paper ){
+    else if (playerSelection == "scissors" && computerSelection == "paper" ){
         return alert("You win! Scissors beats Paper!!");
     }
-    else if (playerSelection == scissors && computerSelection == rock){
+    else if (playerSelection == "scissors" && computerSelection == "rock"){
         return alert("You lose! Rock beats Scissors");
+    } 
+    else {
+
     }
 
 
 }
 
-playRound();
+playRound(playerSelection, computerSelection);
